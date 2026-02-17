1cdf01c9c123d97d0206d342f325e6a8537ec73e
Jakub Paluch <qbusik@gmail.com>
2026-02-17 07:28:19 +0100

    add remaining models and make new migrations

--- library_service/models.py
+++ library_service/models.py
@@ -1,4 +1,8 @@
+from decimal import Decimal
+
 from django.db import models
+
+from user.models import User
 
 
 class Cover(models.TextChoices):
@@ -12,3 +16,32 @@ class Book(models.Model):
     cover = models.CharField(max_length=4, choices=Cover.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class PaymentStatus(models.TextChoices):
+    PENDING = "PENDING", "Pending"
+    PAID = "PAID", "Paid"
+
+
+class PaymentType(models.TextChoices):
+    PAYMENT = "PAYMENT", "Payment"
+    FINE = "FINE", "Fine"
+
+
+class Payment(models.Model):
+    status = models.CharField(max_length=7, choices=PaymentStatus.choices)
+    type = models.CharField(max_length=7, choices=PaymentType.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal("0.00")
+    )
