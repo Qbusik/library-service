7ff39fb2405f282114efde971d2ce35e9fa920dd
Jakub Paluch <qbusik@gmail.com>
2026-02-17 09:26:31 +0100

    add serializers, view and urls for Book

--- library_service/urls.py
+++ library_service/urls.py
@@ -1,2 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from library_service.views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register("books", BookViewSet, basename="books")
+
 app_name = "library_service"
-urlpatterns = []
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- library_service/views.py
+++ library_service/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from library_service.models import Book
+from library_service.serializers import BookListSerializer, BookDetailSerializer
+
+
+class StandardPagination(PageNumberPagination):
+    page_size = 20
+    max_page_size = 100
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookDetailSerializer
+    pagination_class = StandardPagination
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAdminUser()]
+        return super().get_permissions()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookDetailSerializer
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        return queryset.distinct()
