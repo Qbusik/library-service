deb6a818d7c2207b9968ee6456bd248f2341d07c
Jakub Paluch <qbusik@gmail.com>
2026-02-17 07:31:55 +0100

    register models in admin

--- library_service/admin.py
+++ library_service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from library_service.models import Borrowing, Book, Payment
+
+admin.site.register(Book)
+admin.site.register(Borrowing)
+admin.site.register(Payment)
