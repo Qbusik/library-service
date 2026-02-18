7311563a96c4a86fc8f49764f0948ce9f4c1c5a9
Jakub Paluch <qbusik@gmail.com>
2026-02-18 06:56:17 +0100

    add validations for borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from books.models import Book
@@ -7,7 +10,7 @@ from user.models import User
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -16,3 +19,14 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"Borrow: {self.borrow_date} -> {self.expected_return_date}"
+
+    def clean(self):
+        if self.borrow_date < date.today():
+            raise ValidationError({"borrow_date": "Borrow date cannot be in the past."})
+
+        if self.expected_return_date < self.borrow_date:
+            raise ValidationError(
+                {
+                    "expected_return_date": "Expected return date must be after borrow date."
+                }
+            )
