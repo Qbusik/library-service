946efbbf4fa82d1b6d0c91ee1e6e105cc05dc0d7
Jakub Paluch <qbusik@gmail.com>
2026-02-20 06:44:15 +0100

    add PaymentDetailSerializer to payment retrieve view

--- payments/views.py
+++ payments/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from books.views import StandardPagination
 from payments.models import Payment
-from payments.serializers import PaymentListSerializer
+from payments.serializers import PaymentListSerializer, PaymentDetailSerializer
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
@@ -19,3 +19,8 @@ class PaymentsViewSet(viewsets.ModelViewSet):
             return queryset
 
         return queryset.filter(borrowing__user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+        return PaymentListSerializer
