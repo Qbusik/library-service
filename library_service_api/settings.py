b5e7a098f71e4381f5ffcc38a679f7671b0b3587
Jakub Paluch <qbusik@gmail.com>
2026-02-22 09:09:29 +0100

    move django secret key to .env file

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -12,7 +12,7 @@ load_dotenv(BASE_DIR / ".env")
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#!jb*2k6wpv%t+(xcbdw+pwsjty8md(9wubh=faacu^3my+1@p"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
